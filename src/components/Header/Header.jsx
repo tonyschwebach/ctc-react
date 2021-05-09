e12ae62b5209b1ad22b8dc4a8aa3c81d431b1f46
T Schway <tony.schwebach@gmail.com>
2021-05-09 15:29:31 -0400

    styled navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, DropdownButton } from "react-bootstrap";
 import { withRouter } from "react-router";
 import "./Header.css";
 
@@ -7,64 +7,71 @@ const Header = (props) => {
   const { location } = props;
 
   return (
-      <Navbar bg="dark" variant="dark" id="navbar">
-        <Navbar.Brand href="/">Cold Truth Culture</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto" activeKey={location.pathname}>
-            <Nav.Link href="/" activeClassName="active">
-              Home
-            </Nav.Link>
-            <Nav.Link href="/about" activeClassName="active">
-              About
-            </Nav.Link>
-            <Nav.Link href="/cold" activeClassName="active">
-              $COLD
-            </Nav.Link>
-            <Nav.Link href="/education" activeClassName="active">
-              Education
-            </Nav.Link>
-            <Nav.Link href="/rewards" activeClassName="active">
-              Rewards
-            </Nav.Link>
-            <NavDropdown title="Join Us" id="basic-nav-dropdown">
-              <NavDropdown.Item
-                href="https://discord.gg/GSmaQYuwcZ"
-                target="_blank"
-              >
-                Discord
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                href="https://twitter.com/ColdTruthCult"
-                target="_blank"
-              >
-                Twitter
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                href="https://t.me/ColdTruthCultureNFTs"
-                target="_blank"
-              >
-                Telegram
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                href="mailto:crosschainapplications@gmail.com"
-                target="_blank"
-              >
-                Email
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#" target="_blank">
-                Governance
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar variant="dark" id="navbar" expand="lg" collapseOnSelect>
+      <Navbar.Brand href="/" className="ml-3">
+        Cold <span className="truth_green">Truth</span> Culture
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto" activeKey={location.pathname}>
+          <Nav.Link href="/" className="mx-1">
+            Home
+          </Nav.Link>
+          <Nav.Link href="/about" className="mx-1">
+            About
+          </Nav.Link>
+          <Nav.Link href="/cold" className="mx-1">
+            $COLD
+          </Nav.Link>
+          <Nav.Link href="/education" className="mx-1">
+            Education
+          </Nav.Link>
+          <Nav.Link href="/rewards" className="mx-1">
+            Rewards
+          </Nav.Link>
 
+          <DropdownButton
+            title="Join Us"
+            id="collapsible-nav-dropdown"
+            menuAlign="right"
+            variant="light"
+            className="mr-3 ml-1 my-auto"
+          >
+            <NavDropdown.Item
+              href="https://discord.gg/GSmaQYuwcZ"
+              target="_blank"
+            >
+              Discord
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              href="https://twitter.com/ColdTruthCult"
+              target="_blank"
+            >
+              Twitter
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              href="https://t.me/ColdTruthCultureNFTs"
+              target="_blank"
+            >
+              Telegram
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              href="mailto:crosschainapplications@gmail.com"
+              target="_blank"
+            >
+              Email
+            </NavDropdown.Item>
+            {/* <NavDropdown.Divider />
+            <NavDropdown.Item href="#" target="_blank">
+              Governance
+            </NavDropdown.Item> */}
+          </DropdownButton>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
-const HeaderWithRouter = withRouter(Header)
-
+const HeaderWithRouter = withRouter(Header);
 
 export default HeaderWithRouter;
