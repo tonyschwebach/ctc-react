e7082bae343f91bcf7fb8e2cfa6c64cad9c2fbf3
T Schway <tony.schwebach@gmail.com>
2021-05-10 11:07:50 -0400

    added druk-wide font

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,25 +9,28 @@ const Hero = () => {
   return (
     <div>
       <Jumbotron id="hero">
-        <Container>
-          <Row>
-            <Col md={7}>
+        <Row id="hero_body">
+          <Col lg={7}>
+            <Container>
               <h1>Cold Truth Culture -</h1>
               <h2>
                 A Community That <br />
                 Empowers NFT Artists
               </h2>
               <Button variant="light">Learn more</Button>
-            </Col>
-            <Col lg={5}>
-              <Container id="featured_art_container">
-                <img src={cat} alt="cat" id="hero_cat" />
-                <img src={yinyang} alt="yin yang" id="hero_yinyang" />
-                <img src={tv} alt="tv" id="hero_tv" />
-              </Container>
-            </Col>
-          </Row>
-        </Container>
+            </Container>
+          </Col>
+          <Col lg={5}>
+            <Container id="featured_art_container">
+              <img src={cat} alt="cat" id="hero_cat" />
+              <img src={yinyang} alt="yin yang" id="hero_yinyang" />
+              <img src={tv} alt="tv" id="hero_tv" />
+            </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Col id="hero_footer">Cold Truth Culture</Col>
+        </Row>
       </Jumbotron>
     </div>
   );
