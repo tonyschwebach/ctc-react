82674a7948400b9755fe8924e68e2a5db7b7e38c
T Schway <tony.schwebach@gmail.com>
2021-05-12 15:07:28 -0400

    add text content to about us

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,66 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      About page
+      <h1>About Us</h1>
+      <p>
+        <strong> About Cold Truth Culture</strong>
+      </p>
+      <p>
+        Since the beginning of time, every culture from all around of the world
+        has appreciated and dedicated time to creating and collecting meaningful
+        works of art. Until now, there have been a number of barriers holding
+        artists back from sharing, exchanging, and getting recognition for their
+        artwork all around the globe. Limited means to increase exposure for
+        artist's work can prevent collectors and enthusiasts from ever seeing,
+        purchasing, or admiring such art. There are thousands of incredibly
+        talented artists from 2nd and 3rd world countries who may never get the
+        chance to share their craft despite the decades of experience they have.
+        These artists may lack the initial capital and education required to set
+        up an online store in order to begin to sell and market their own work.
+        Even seemingly small purchases from 1st world countries could go a long
+        way for these artist's families and wellbeing. Another barrier is the
+        difficulty for the unbanked population to acquire cryptocurrency without
+        access to financial services or KYC requirements. Without having an
+        issued means of Identification in some countries, it may be impossible
+        to get started with cryptocurrency in order to get equal opportunities
+        as all of the other NFT artists on the blockchain.
+      </p>
+      <p>
+        <strong>This is where Cold Truth Culture steps in...</strong>
+      </p>
+      <p>
+        We offer the chance for artists of all skill levels to begin their
+        journey with digital artwork on the blockchain as well as provide them
+        with an incubator of sorts to help kick start their career. CTC artists
+        are paid in our native cryptocurrency (COLD) for submitting artwork and
+        promoting the artwork in our NFT farms. We provide them with the tools
+        and advice to create their very own projects and use our resources to
+        get recognition among the community. Some artists have even received
+        requests for paid work.
+      </p>
+      <p>
+        <strong>
+          Here is your chance to create, capture, and collect the unseen artwork
+          of the world...
+        </strong>
+      </p>
+      <br />
+
+      <p>
+        Pioneers in the early stages of the new digital blockchain age within
+        popular media, the Cold Truth Culture NFT community has solidified
+        itself within the Unifty NFT environment by creating a decentralised
+        autonomous organisation on the xDai chain for the benefit of all
+        crypto-artists. The Cold Truth Cash token ($COLD) has attracted many
+        experienced artists as well as newcomers into the crypto NFT space from
+        all over the world and united them on a mission to spread
+        crypto-knowledge to a new generation of artists and enthusiasts. The
+        Cold Truth Culture is a major part of this NFT phenomenon by
+        simultaneously increasing its value for artists, the consumers, and the
+        crypto-world: the only place where this can be possible.
+      </p>
     </div>
   );
 };
