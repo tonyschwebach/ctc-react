08c3bea0adf0c622e7aa7c42842ac67867dc9da4
T Schway <tony.schwebach@gmail.com>
2021-05-16 17:39:01 -0400

    responsiveness for 3 cats

--- src/components/Partnerships/Partnerships.jsx
+++ src/components/Partnerships/Partnerships.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import partners from "./partners.json";
 import Partner from "./Partner/Partner";
 import catBlue from "../../Assets/images/art/cat_3_blue.png";
@@ -11,10 +11,27 @@ const Partnerships = () => {
   return (
     <div>
       <Row id="partnerships" className="justify-content-center">
-        <Col lg={5}>
-          <img src={catBlue} alt="blue cat" id="blue-cat" />
-          <img src={catGreen} alt="green cat" id="green-cat" />
-          <img src={catPink} alt="pink cat"  id="pink-cat"/>
+        <Col lg={5} className="featured-art-container">
+          <Container id="partner-art">
+            <img
+              src={catBlue}
+              alt="blue cat"
+              id="blue-cat"
+              className="img-move img-fluid"
+            />
+            <img
+              src={catGreen}
+              alt="green cat"
+              id="green-cat"
+              className="img-move img-fluid"
+            />
+            <img
+              src={catPink}
+              alt="pink cat"
+              id="pink-cat"
+              className="img-move img-fluid"
+            />
+          </Container>
         </Col>
         <Col lg={7}>
           <h2 className="mb-5">Our partnerships</h2>
