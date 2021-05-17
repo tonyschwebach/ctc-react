2d8288ea1494bfd25c07d2e076bd21b183ed8f19
T Schway <tony.schwebach@gmail.com>
2021-05-17 16:15:23 -0400

    education coming soon

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import HeaderWithRouter from "../src/components/Header/Header";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Rewards from "./pages/Rewards/Rewards";
+import Footer from "./components/Footer/Footer";
+import Education from "./pages/Education/Education";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <HeaderWithRouter />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/rewards" component={Rewards} />
-          <Route path="*" component={Home} />
-        </Switch>
+        <div className="page">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/rewards" component={Rewards} />
+            <Route path="/education" component={Education} />
+            <Route path="*" component={Home} />
+          </Switch>
+        </div>
         <Footer />
       </Router>
     </div>
---
+++ src/pages/Education/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Education = () => {
+  return (
+    <Container className="mt-5">
+      <h2>
+        <strong>
+          Education
+        </strong>
+      </h2>
+      <h3 className="truth-green">Coming Soon</h3>
+    </Container>
+  );
+};
+
+export default Education;
