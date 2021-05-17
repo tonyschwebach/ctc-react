7327738fa78ba1106cfb8a639fa9a14149b334ce
T Schway <tony.schwebach@gmail.com>
2021-05-17 16:24:45 -0400

    added art to education

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import mirror from "../../Assets/images/art/mirror.jpg";
+import './Education.css'
 
 const Education = () => {
   return (
     <Container className="mt-5">
       <h2>
-        <strong>
-          Education
-        </strong>
+        <strong>Education</strong>
       </h2>
       <h3 className="truth-green">Coming Soon</h3>
+      <img src={mirror} alt="trans dimensional mirror" className='img-fluid rounded shadow-lg mt-5' />
     </Container>
   );
 };
